7fcc8de323fb64e67fd181971a64ceba0511102a
MamatovY <mamatovyiman28@gmail.com>
2024-02-21 20:13:06 +0600

    add database

--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,23 @@
 import mongoose from 'mongoose'
 
-let isConnect = false //отслеживание соединения
+let isConnect = false //отслеживание соединения 
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true)
+
+    if (isConnect) {
+        console.log('MongoDB is already connected');
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "share_prompt",
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('MongoDB is connected');
+        isConnect = true
+    } catch (error) {
+        console.log(error);
+    }
+}
